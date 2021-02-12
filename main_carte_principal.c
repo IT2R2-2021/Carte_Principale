115b3319d231e9f9f4d6d3307577818e3d1f256d
Matthieu VIAUD <43612203+Mirague94@users.noreply.github.com>
2021-02-12 08:33:38 +0100

    INITIALISATION

---
+++ main_carte_principal.c
@@ -0,0 +1,25 @@
+/*
+Projet : Voiture Autonome
+Carte : Principal
+Groupe : IT2R2
+Contributeur :
+	Matthieu
+*/
+
+#include "Driver_CAN.h"                 // ::CMSIS Driver:CAN
+
+//variable globale
+
+//Prototype de fonction
+
+//Main
+int main()
+{
+	//Variable
+	
+	//Initialisation
+	
+	while(1)
+	{
+	}
+}
